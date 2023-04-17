0bbcc7c929377b46795c5b2b09973670e2d8930c
Dm Sk <johnlankasterpek@yandex.ru>
2023-04-17 12:33:49 +0300

    Аннотация типов

--- some_file.py
+++ some_file.py
@@ -4,3 +4,9 @@ print("Это первая ветка в GitHab")
 print('These are new local changes')
 
 print('Проверка Git из Новочебоксарска')
+
+def say_something(number: int, word: str) -> str:
+    word = word.capitalize()
+    return word * number
+
+print (say_something(11, 'hello'))
